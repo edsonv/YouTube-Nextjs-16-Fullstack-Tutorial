9b30bc225def2564e5c4c2123eb20f4e0df02b18
Edson J. Vargas <edson.jhair@yahoo.com>
2026-01-16 23:53:25 -0500

    Disable cache directives for first deploy

--- app/(shared-layout)/blog/page.tsx
+++ app/(shared-layout)/blog/page.tsx
@@ -4,9 +4,9 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
 import { fetchQuery } from "convex/nextjs";
 import { Metadata } from "next";
-import { cacheLife, cacheTag } from "next/cache";
 import Image from "next/image";
 import Link from "next/link";
+import { connection } from "next/server";
 import { Suspense } from "react";
 
 // export const dynamic = "force-static";
@@ -38,9 +38,10 @@ export default async function BlogPage() {
 }
 
 async function LoadBlogList() {
-  "use cache";
-  cacheLife("hours");
-  cacheTag("blog");
+  // "use cache";
+  // cacheLife("hours");
+  // cacheTag("blog");
+  await connection();
   const data = await fetchQuery(api.posts.getPosts);
 
   return (
