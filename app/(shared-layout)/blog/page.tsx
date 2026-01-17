7fcf66c9f260752a366b6d425004d70a48a8d719
Edson J. Vargas <edson.jhair@yahoo.com>
2026-01-16 23:56:48 -0500

    Reenable cache directives

--- app/(shared-layout)/blog/page.tsx
+++ app/(shared-layout)/blog/page.tsx
@@ -4,6 +4,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
 import { fetchQuery } from "convex/nextjs";
 import { Metadata } from "next";
+import { cacheLife, cacheTag } from 'next/cache';
 import Image from "next/image";
 import Link from "next/link";
 import { connection } from "next/server";
@@ -38,10 +39,9 @@ export default async function BlogPage() {
 }
 
 async function LoadBlogList() {
-  // "use cache";
-  // cacheLife("hours");
-  // cacheTag("blog");
-  await connection();
+  "use cache";
+  cacheLife("hours");
+  cacheTag("blog");
   const data = await fetchQuery(api.posts.getPosts);
 
   return (
