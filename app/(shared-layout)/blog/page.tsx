a0bd15d5a5ee14823a8175d0d551e9c07dd4b305
Edson J. Vargas <edson.jhair@yahoo.com>
2026-01-16 23:47:21 -0500

    Trying to fix vercel deploy, reenables cache directives

--- app/(shared-layout)/blog/page.tsx
+++ app/(shared-layout)/blog/page.tsx
@@ -4,6 +4,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
 import { fetchQuery } from "convex/nextjs";
 import { Metadata } from "next";
+import { cacheLife, cacheTag } from "next/cache";
 import Image from "next/image";
 import Link from "next/link";
 import { Suspense } from "react";
@@ -37,6 +38,9 @@ export default async function BlogPage() {
 }
 
 async function LoadBlogList() {
+  "use cache";
+  cacheLife("hours");
+  cacheTag("blog");
   const data = await fetchQuery(api.posts.getPosts);
 
   return (
